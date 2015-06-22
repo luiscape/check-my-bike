41870823ff396aa82c044d3da94774e4d0cc5120
Luis Capelo <luiscape@gmail.com>
2015-06-21 20:02:46 -0400

    adding sandman to the setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,8 +8,14 @@ source venv/bin/activate
 pip install -r requirements.txt
 pip install requests[security]
 
+## Installing sandman from source.
+git clone https://github.com/jeffknupp/sandman
+cd sandman
+python setup.py install
+rm -rf sandman
+
 #
 # Configuring application.
 #
-python scripts/setup_app/
-python scripts/citi_download/
+# python scripts/setup_app/
+# python scripts/citi_download/
