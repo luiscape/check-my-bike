e2ce0121198131b3c0f4a52cccdb9dae908f5b55
Luis Capelo <luiscape@gmail.com>
2015-07-22 21:10:33 -0400

    adding wrappers around the location scraper

---
+++ scripts/locations_scraper/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+dir = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
+sys.path.append(dir)
+
+from utilities.prompt_format import item
+from locations_scraper.scraper import Main as ScraperMain
+
+
+def Main():
+  '''Wrapper.'''
+
+  ScraperMain()
+
+
+if __name__ == '__main__':
+  Main()
