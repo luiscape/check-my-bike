0ae07de0c57433b0aadc72b840704e9c0186d433
Luis Capelo <luiscape@gmail.com>
2015-08-20 21:11:42 -0400

    refactoring app call for scheduler

--- app/__main__.py
+++ app/__main__.py
@@ -2,26 +2,17 @@
 # # -*- coding: utf-8 -*-
 
 import sys
-import server
 import run_scheduler
+from termcolor import colored as color
 
+#
+# Run the scheduler.
+#
 if __name__ == '__main__':
 
-  #
-  # If command provided
-  # run the collector.
-  #
-  try:
-    command = sys.argv[1]
+  print '--------------------------'
+  print '|    %s    |' % color("ROLLTIME COLLECT", "blue", attrs=['bold'])
+  print '|       [ %s ]      |' % color("v.0.1.0", "yellow", attrs=['bold'])
+  print '--------------------------'
 
-    if command == 'scheduler':
-      run_scheduler.RunScheduler()
-
-  except Exception as e:
-    pass
-
-  #
-  # If no command
-  # provided run API server.
-  #
-  server.Main()
+  run_scheduler.RunScheduler()
