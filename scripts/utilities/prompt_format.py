4245dccab91236bd1f1f66e5ce885a17ea40696c
Luis Capelo <luiscape@gmail.com>
2015-08-30 11:32:24 -0400

    adding encoding to non-ancii characters

--- scripts/utilities/prompt_format.py
+++ scripts/utilities/prompt_format.py
@@ -5,12 +5,12 @@ from termcolor import colored as color
 
 def item(i):
   dictionary = {
-    'prompt_bullet': color(" →", "blue", attrs=['bold']),
-    'prompt_error':  color(" ERROR:", "red", attrs=['bold']),
-    'prompt_success': color(" SUCCESS:", "green", attrs=['bold']),
-    'prompt_warn': color(" WARN:", "yellow", attrs=['bold'])
+    'prompt_bullet': color(u" →", "blue", attrs=['bold']),
+    'prompt_error':  color(u" ERROR:", "red", attrs=['bold']),
+    'prompt_success': color(u" SUCCESS:", "green", attrs=['bold']),
+    'prompt_warn': color(u" WARN:", "yellow", attrs=['bold'])
   }
-  return dictionary[i].decode('utf-8')
+  return dictionary[i].encode('utf-8')
 
 
 if __name__ == '__main__':
