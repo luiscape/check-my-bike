346b3f4c3529771cf2bca8561244b47665c7c300
Luis Capelo <luiscape@gmail.com>
2015-09-02 22:36:42 -0400

    adding notification send to node watch

--- scripts/station_collector/__main__.py
+++ scripts/station_collector/__main__.py
@@ -7,8 +7,8 @@ import sys
 dir = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 sys.path.append(dir)
 
+from status.send import SendStatus
 from utilities.prompt_format import item
-
 from citibike_station.fetch import FetchLatestStationData
 
 
@@ -31,5 +31,11 @@ def Main():
     print '%s Failed to fetch data from the CitiBike API.' % item('prompt_error')
     return False
 
+
 if __name__ == '__main__':
-  Main()
+
+  if Main() == False:
+    SendStatus('error')
+
+  else:
+    SendStatus('ok')
