78e1531f7ce76a460e7438f393a6776d8a56df0a
Luis Capelo <luiscape@gmail.com>
2015-09-09 19:28:34 -0400

    linting

--- tests/test_setup_app.py
+++ tests/test_setup_app.py
@@ -1,20 +1,19 @@
-# #!/usr/bin/python
-# # -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# import os
-# import sys
-# import mock
-# import unittest
-# import scraperwiki
-# from mock import patch
+import os
+import sys
+import mock
+import unittest
+from mock import patch
 
-# from scripts.utilities import load_config as Config
-# from scripts.setup_app import db as Setup
+from scripts.utilities import load_config as Config
+from scripts.setup_app import db as Setup
 
 
-# class TestSetupDb(unittest.TestCase):
-#   '''Testing the process of creating a database.'''
+class TestSetupDb(unittest.TestCase):
+  '''Testing the process of creating a database.'''
 
-#   def test_setup_db_doesnt_fail(self):
-#     assert CreateDbAndTable() != False
+  def test_setup_db_doesnt_fail(self):
+    assert Setup.CreateDbAndTable() != False
 
