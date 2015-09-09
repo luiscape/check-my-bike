dc237382eb7767c60e005e0b869fb5e89a5a8dd8
Luis Capelo <luiscape@gmail.com>
2015-09-09 19:28:12 -0400

    linting

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -1,18 +1,18 @@
-# #!/usr/bin/python
-# # -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# import os
-# import sys
-# import mock
-# import unittest
-# from mock import patch
+import os
+import sys
+import mock
+import unittest
+from mock import patch
 
-# from scripts.utilities import load_config as Config
-# from scripts.station_collector import fetch as Fetch
-# from scripts.station_collector import process as Process
+from scripts.utilities import load_config as Config
+from scripts.station_collector import fetch as Fetch
+from scripts.station_collector import process as Process
 
-# class TestFetchDAta(unittest.TestCase):
-#   '''Testing that data is fetched successfully.'''
+class TestFetchDAta(unittest.TestCase):
+  '''Testing that data is fetched successfully.'''
 
-#   def test_that_default_parameters_dont_fail(self):
-#     assert Fetch.FetchLatestStationData() != False
+  def test_that_default_parameters_dont_fail(self):
+    assert Fetch.FetchLatestStationData() != False
