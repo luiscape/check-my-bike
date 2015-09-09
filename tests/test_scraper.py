f8238f9d0b2823bced4d39195012ebb7fb4be63a
Luis Capelo <luiscape@gmail.com>
2015-09-09 19:28:27 -0400

    linting

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,20 +1,20 @@
-# # #!/usr/bin/python
-# # # -*- coding: utf-8 -*-
+# #!/usr/bin/python
+# # -*- coding: utf-8 -*-
 
-# import os
-# import sys
-# import mock
-# import unittest
-# from mock import patch
+import os
+import sys
+import mock
+import unittest
+from mock import patch
 
-# from scripts.utilities import load_config as Config
-# from scripts.locations_scraper import scraper as Scraper
+from scripts.utilities import load_config as Config
+from scripts.locations_scraper import scraper as Scraper
 
 
-# class CheckMotivateScraper(unittest.TestCase):
-#   '''Tests that the scraper to Motivate's website works as expected.'''
+class CheckMotivateScraper(unittest.TestCase):
+  '''Tests that the scraper to Motivate's website works as expected.'''
 
-#   def test_wrapper_function_doesnt_fail(self):
-#     assert Scraper.ScrapeMotivateWebsite() != False
+  def test_wrapper_function_doesnt_fail(self):
+    assert Scraper.ScrapeMotivateWebsite() != False
 
 
